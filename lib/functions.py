9e9ec96a9ba26ba01006e3ea0e5a2535ec2874ed
Ndet0 <festusndet00@gmail.com>
2025-11-19 13:56:35 +0300

    initial commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    if isinstance(number, int) or isinstance(number, float):
+        return number / 2
+
+
+if __name__ == "__main__":
+    greet_programmer()
+    greet("Guido")
+    greet_with_default()
+    add(45, 55)
+    # number = int(input("Enter a number to halve: "))
+    halve(99)
